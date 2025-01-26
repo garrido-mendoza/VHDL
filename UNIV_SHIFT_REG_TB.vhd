c64a1c4775b6edcd4dc175039b570506ec2e135f
garrido-mendoza <garridomendoza@gmail.com>
2025-01-26 02:42:05 -0800

    Update and rename TB_UNIV_SHIFT_REG.vhd to UNIV_SHIFT_REG_TB.vhd

--- UNIV_SHIFT_REG_TB.vhd
+++ UNIV_SHIFT_REG_TB.vhd
@@ -1,35 +1,39 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
+use IEEE.NUMERIC_STD.all;
 
 entity univ_shift_reg_tb is
 end univ_shift_reg_tb;
 
 architecture Behavioral of univ_shift_reg_tb is
 
-component univ_shift_reg is
-    generic (   data_width : integer := 8);
-    port (  clk : in std_logic;
+    component univ_shift_reg is
+        generic (data_width : integer := 8);
+        port (
+            clk : in std_logic;
             rst : in std_logic;  
             I   : in std_logic_vector(data_width - 1 downto 0);
             S   : in std_logic_vector(data_width - 7 downto 0); -- Shift instruction. 
-            A   : out std_logic_vector(data_width - 1 downto 0));
-end component;
+            A   : out std_logic_vector(data_width - 1 downto 0)
+        );
+    end component;
 
-signal clk_i : std_Logic;
-signal rst_i : std_Logic;
-signal I_i : std_logic_vector(7 downto 0);
-signal S_i : std_logic_vector(1 downto 0);
-signal A_o : std_logic_vector(7 downto 0);
+    signal clk_i : std_logic;
+    signal rst_i : std_logic;
+    signal I_i : std_logic_vector(7 downto 0);
+    signal S_i : std_logic_vector(1 downto 0);
+    signal A_o : std_logic_vector(7 downto 0);
 
 begin
 
     uut : univ_shift_reg
-    port map (  clk => clk_i,
-                rst => rst_i,
-                I   => I_i, 
-                S   => S_i, 
-                A   => A_o);
+        port map (
+            clk => clk_i,
+            rst => rst_i,
+            I   => I_i, 
+            S   => S_i, 
+            A   => A_o
+        );
 
     clk_gen : process
     begin
@@ -50,7 +54,6 @@ begin
         
         wait for 50 us;
         rst_i <= '0';
-        
         
         wait for 100 us;
         S_i <= "00";
