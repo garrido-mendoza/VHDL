4ba93a8a1a2e28350a7602e1b3a7a55195502006
garrido-mendoza <garridomendoza@gmail.com>
2025-01-26 02:38:01 -0800

    Update and rename TB_THERMOSTAT_CNTR_FSM.vhd to THERMOSTAT_CNTR_FSM_TB.vhd

--- THERMOSTAT_CNTR_FSM_TB.vhd
+++ THERMOSTAT_CNTR_FSM_TB.vhd
@@ -7,55 +7,59 @@ end thermostat_tb;
 
 architecture Behavioral of thermostat_tb is
 
-component thermostat is
-    port (    clk             : in std_logic;
-              rst             : in std_logic;
-              current_temp    : in std_logic_vector(6 downto 0);
-              desired_temp    : in std_logic_vector(6 downto 0);
-              display_select  : in std_logic;
-              cool            : in std_logic;   
-              heat            : in std_logic;   
-              furnace_hot     : in std_logic;   
-              ac_ready        : in std_logic;   
-              temp_display    : out std_logic_vector(6 downto 0);
-              a_c_on          : out std_logic;  
-              furnace_on      : out std_logic;  
-              fan_on          : out std_logic);
-end component;
+    component thermostat is
+        port (
+            clk             : in std_logic;
+            rst             : in std_logic;
+            current_temp    : in std_logic_vector(6 downto 0);
+            desired_temp    : in std_logic_vector(6 downto 0);
+            display_select  : in std_logic;
+            cool            : in std_logic;   
+            heat            : in std_logic;   
+            furnace_hot     : in std_logic;   
+            ac_ready        : in std_logic;   
+            temp_display    : out std_logic_vector(6 downto 0);
+            a_c_on          : out std_logic;  
+            furnace_on      : out std_logic;  
+            fan_on          : out std_logic
+        );
+    end component;
 
--- Input signal declarations            
-signal clk_i : std_logic;
-signal rst_i : std_logic := '0';
-signal current_temp_i : std_logic_vector(6 downto 0) := "0000000";
-signal desired_temp_i : std_logic_vector(6 downto 0) := "0000000";
-signal display_select_i : std_logic := '0';
-signal cool_i : std_logic := '0';
-signal heat_i : std_logic := '0';
-signal furnace_hot_i : std_logic := '0'; 
-signal ac_ready_i : std_logic := '0';
+    -- Input signal declarations            
+    signal clk_i             : std_logic;
+    signal rst_i             : std_logic := '0';
+    signal current_temp_i    : std_logic_vector(6 downto 0) := "0000000";
+    signal desired_temp_i    : std_logic_vector(6 downto 0) := "0000000";
+    signal display_select_i  : std_logic := '0';
+    signal cool_i            : std_logic := '0';
+    signal heat_i            : std_logic := '0';
+    signal furnace_hot_i     : std_logic := '0'; 
+    signal ac_ready_i        : std_logic := '0';
 
--- Output signal declarations
-signal temp_display_o : std_logic_vector(6 downto 0);
-signal a_c_on_o : std_logic;
-signal furnace_on_o : std_logic;
-signal fan_on_o : std_logic;
+    -- Output signal declarations
+    signal temp_display_o    : std_logic_vector(6 downto 0);
+    signal a_c_on_o          : std_logic;
+    signal furnace_on_o      : std_logic;
+    signal fan_on_o          : std_logic;
 
 begin
 
     uut : thermostat
-    port map (  clk             => clk_i,
-                rst             => rst_i,
-                current_temp    => current_temp_i,
-                desired_temp    => desired_temp_i,
-                display_select  => display_select_i,
-                cool            => cool_i,
-                heat            => heat_i,
-                furnace_hot     => furnace_hot_i,
-                ac_ready        => ac_ready_i,   
-                temp_display    => temp_display_o,
-                a_c_on          => a_c_on_o,
-                furnace_on      => furnace_on_o,
-                fan_on          => fan_on_o);
+        port map (
+            clk             => clk_i,
+            rst             => rst_i,
+            current_temp    => current_temp_i,
+            desired_temp    => desired_temp_i,
+            display_select  => display_select_i,
+            cool            => cool_i,
+            heat            => heat_i,
+            furnace_hot     => furnace_hot_i,
+            ac_ready        => ac_ready_i,   
+            temp_display    => temp_display_o,
+            a_c_on          => a_c_on_o,
+            furnace_on      => furnace_on_o,
+            fan_on          => fan_on_o
+        );
     
     clk_gen : process
     begin
@@ -67,22 +71,20 @@ begin
         end loop;
     end process;  
     
-                 
     stimulus : process
-    
-    -- Procedure: Set Temps.
-    procedure set_temps (current, desired : in integer) is
-    begin
-        current_temp_i <= std_logic_vector(to_unsigned(current, 7));
-        desired_temp_i <= std_logic_vector(to_unsigned(desired, 7));
-        return;
-    end;     
+        -- Procedure: Set Temps.
+        procedure set_temps (current, desired : in integer) is
+        begin
+            current_temp_i <= std_logic_vector(to_unsigned(current, 7));
+            desired_temp_i <= std_logic_vector(to_unsigned(desired, 7));
+            return;
+        end;     
 
-    variable ac_on_event : time;
-    variable ac_off_event : time;
-    variable fan_on_event : time;
-    variable fan_off_event : time;
-    variable time_span : time;
+        variable ac_on_event : time;
+        variable ac_off_event : time;
+        variable fan_on_event : time;
+        variable fan_off_event : time;
+        variable time_span : time;
 
     begin
         report "Starting the thermostat simulation.";
